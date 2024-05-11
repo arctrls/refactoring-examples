663f3924714e379469bd827389435c6a61fd1e9d
이기원/백엔드개발팀 <jake@ktown4u.com>
2024-05-11 23:30:06 +0900

    first commit.

---
+++ src/main/java/com/arctrls/RefactoringExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.arctrls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringExamplesApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(RefactoringExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arctrls/RefactoringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arctrls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
