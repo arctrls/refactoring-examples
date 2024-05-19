b5148b22ad3b8ff0dbf64962eeb0d054d26f7b52
Kiwon Lee <bwv1050@gmail.com>
2024-05-19 11:47:44 +0900

    2. add domain class Coupon, add a new method CouponDao::findById.

--- src/main/java/com/arctrls/timedependentsqlquery/ReadCouponService.java
+++ src/main/java/com/arctrls/timedependentsqlquery/ReadCouponService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.Optional;
 
 
 @Service
