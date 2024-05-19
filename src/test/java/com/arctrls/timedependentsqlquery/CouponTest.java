1fd88a685c7e383e7ba4967f415d1586943209e3
Kiwon Lee <bwv1050@gmail.com>
2024-05-19 12:01:41 +0900

    7. add a failing unit test case: isDownloadable_ShouldReturnTrue_WhenTimeIsDownloadable.

--- src/test/java/com/arctrls/timedependentsqlquery/CouponTest.java
+++ src/test/java/com/arctrls/timedependentsqlquery/CouponTest.java
@@ -2,7 +2,10 @@ package com.arctrls.timedependentsqlquery;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CouponTest {
 
@@ -12,5 +15,17 @@ class CouponTest {
 
         assertThatThrownBy(() -> coupon.isDownloadable(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isDownloadable_ShouldReturnTrue_WhenTimeIsDownloadable() {
+        final var availableFrom = Instant.parse("2021-12-25T00:00:00Z");
+        final var availableTo = Instant.parse("2021-12-25T23:59:59Z");
+        final var currentTime = Instant.parse("2021-12-25T12:00:00Z");
+        final var coupon = new Coupon(availableFrom, availableTo);
+
+        final var isDownloadable = coupon.isDownloadable(currentTime);
+
+        assertTrue(isDownloadable);
     }
 }
