02a6321ce0287616f3341a1bc23f5b500b12dc5a
Kiwon Lee <bwv1050@gmail.com>
2024-05-19 12:10:25 +0900

    14. remove unused method.

--- src/main/java/com/arctrls/timedependentsqlquery/CouponDao.java
+++ src/main/java/com/arctrls/timedependentsqlquery/CouponDao.java
@@ -3,19 +3,10 @@ package com.arctrls.timedependentsqlquery;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import java.time.Instant;
 import java.util.Optional;
 
 @Mapper
 interface CouponDao {
-
-    @Select("""
-            select exists(select id
-                                  from coupon
-                                  where id = #{couponId}
-                                    and #{now} between start_dt and end_dt)
-            """)
-    boolean isDownloadable(final Long couponId, final Instant now);
 
     @Select("""
             select id, name, start_dt availableFrom, end_dt availableTo
